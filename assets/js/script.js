60c3f97a389cbab839f6da858f29934a77d7faab
Bilal484 <abilalbro007@gmail.com>
2024-09-10 17:31:09 +0500

    Bilal || Update

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+function showSection(section) {
+    var sections = document.querySelectorAll('.content-section');
+    sections.forEach(function (sec) {
+        sec.style.display = 'none';
+    });
+    document.getElementById(section).style.display = 'block';
+}
+
+
+
+// Hidden Data 
+
+document.getElementById("show-more-btn").addEventListener("click", function () {
+
+    const hiddenContent = document.querySelectorAll(".hidden-content");
+    hiddenContent.forEach(function (element) {
+        element.style.display = "block";
+    });
+    this.style.display = "none";
+});
+
+
+document.getElementById('show-more-apartments').addEventListener('click', function () {
+    let hiddenContent = document.querySelectorAll('.hidden-content_1');
+    hiddenContent.forEach(function (element) {
+        element.style.display = 'block';
+    });
+    this.style.display = 'none'; 
+});
+
+
+document.getElementById('show-more-houses').addEventListener('click', function () {
+    let hiddenContent = document.querySelectorAll('.hidden-content_2');
+    hiddenContent.forEach(function (element) {
+        element.style.display = 'block';
+    });
+    this.style.display = 'none'; 
+});
